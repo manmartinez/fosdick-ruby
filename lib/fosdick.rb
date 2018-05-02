f8ea695075b2e67637e2174d0531e7db279b9cd0
Gregory R. Everitt <greg@elysiumhealth.com>
2018-05-02 10:44:16 -0400

    Revert "Uses SSLv3 to connect to Fosdick"
    This reverts commit 67bb46048c222f5e8410c85b4e2c1c69cf53e81d.
    That commit is now counter-productive because we are switching to a TLS
    1.2 endpoint.

--- lib/fosdick.rb
+++ lib/fosdick.rb
@@ -34,9 +34,7 @@ module Fosdick
     conn = ::Faraday.new(:url => configuration.host) do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       # faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  :patron do |session|
-        session.ssl_version = 'SSLv3'
-      end
+      faraday.adapter  :patron
     end
 
     unless configuration.username.nil?
